2675db4cc1c748af567e0f3d13b08100486f249d
richardangell <richardangell37@gmail.com>
2021-11-03 16:45:09 +0000

    add small logo to use in docs sidebar

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ exclude_patterns = []
 html_theme = "sphinx_rtd_theme"
 
 # logo to appear in the top left home page box
-html_logo = "../../logo.png"
+html_logo = "../../logo-small.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
