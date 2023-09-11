33802afbd41ba6ec05890c939b600ddc21d224b6
davidhopkinson26 <89029024+davidhopkinson26@users.noreply.github.com>
2023-09-11 11:21:34 +0000

    changed MappingTransformerMixin to use pd.Series.replace which removes the need for the ReturnKeyDict class for handling unmapped values.

--- tests/mapping/test_BaseMappingTransformMixin.py
+++ tests/mapping/test_BaseMappingTransformMixin.py
@@ -60,24 +60,24 @@ class TestTransform:
         ):
             x.transform(df)
 
-    def test_pd_series_map_call(self, mocker):
-        """Test the call to pd.Series.map."""
-        spy = mocker.spy(pd.Series, "map")
+    def test_pd_series_replace_call(self, mocker):
+        """Test the call to pd.Series.replace."""
+        spy = mocker.spy(pd.Series, "replace")
 
         df = d.create_df_1()
 
         mapping = {
             "a": {1: "a", 2: "b", 3: "c", 4: "d", 5: "e", 6: "f"},
-            "b": {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6},
+            # "b": {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6},
         }
 
         x = BaseMappingTransformMixin()
-        x.columns = ["a", "b"]
+        x.columns = ["a"]  # , "b"]
         x.mappings = mapping
 
         x.transform(df)
 
-        assert spy.call_count == 2, "unexpected number of calls to pd.Series.map"
+        assert spy.call_count == 2, "unexpected number of calls to pd.Series.replace"
 
         call_args = spy.call_args_list[0]
 
@@ -91,7 +91,7 @@ class TestTransform:
         ta.equality.assert_equal_dispatch(
             expected_pos_args,
             call_pos_arg,
-            "positional args in first pd.Series.map call not correct",
+            "positional args in first pd.Series.replace call not correct",
         )
 
         call_args = spy.call_args_list[1]
@@ -106,5 +106,5 @@ class TestTransform:
         ta.equality.assert_equal_dispatch(
             expected_pos_args,
             call_pos_arg,
-            "positional args in second pd.Series.map call not correct",
+            "positional args in second pd.Series.replace call not correct",
         )
