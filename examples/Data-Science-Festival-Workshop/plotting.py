4950399c546273c1647ea813597eeb40d4de5ac1
davidhopkinson26 <89029024+davidhopkinson26@users.noreply.github.com>
2023-09-12 09:01:44 +0000

    added annotations to examples/Data-Science-Festival-Workshop/plotting.py and setup.py

--- examples/Data-Science-Festival-Workshop/plotting.py
+++ examples/Data-Science-Festival-Workshop/plotting.py
@@ -1,16 +1,19 @@
-def one_way_summary_plot(df, column, response="y"):
+import pandas as pd
+
+
+def one_way_summary_plot(df: pd.DataFrame, column: str, response: str = "y") -> None:
     """Function to produce a rough one-way summary plot of a specific column.
 
     Specifically plot averge response (right y axis) and number of records (left
     y axis) by the selected column (x axis).
 
     """
-    agg = df.groupby(column).agg({column: ["count"], "y": ["mean"]})
+    agg = df.groupby(column).agg({column: ["count"], response: ["mean"]})
 
     ax = agg.plot.bar(y=(column, "count"), ylabel="count", figsize=(8, 5))
 
     agg.plot(
-        y=("y", "mean"),
+        y=(response, "mean"),
         style=":",
         marker=".",
         c="k",
