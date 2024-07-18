58b81a649e2e92c3825ffe2657f309cdf1c196b4
davidhopkinson26 <89029024+davidhopkinson26@users.noreply.github.com>
2024-07-18 15:17:53 +0000

    :construction: added tests to BaseDate test modules

--- tests/dates/test_BaseDateTransformer.py
+++ tests/dates/test_BaseDateTransformer.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import pytest
+
+import tests.test_data as d
 from tests.base_tests import (
     ColumnStrListInitTests,
     GenericFitTests,
@@ -8,6 +12,35 @@ from tests.base_tests import (
 
 class GenericDatesTransformTests:
     """Generic tests for Dates Transformers"""
+
+    def test_mismatched_datetypes_error(
+        self,
+        columns,
+        datetime_col,
+        date_col,
+        initialized_transformers,
+    ):
+        "Test that transform raises an error if one column is a date and one is datetime"
+
+        x = initialized_transformers(self.transformer_name)
+
+        df = d.create_date_diff_different_dtypes()
+        # types don't seem to come out of the above function as expected, hard enforce
+        for col in ["date_col_1", "date_col_2"]:
+            df[col] = pd.to_datetime(df[col]).dt.date
+
+        for col in ["datetime_col_1", "datetime_col_2"]:
+            df[col] = pd.to_datetime(df[col])
+
+        present_types = (
+            {"datetime64", "date"} if datetime_col == 0 else {"date", "datetime64"}
+        )
+        msg = rf"Columns fed to datetime transformers should be \['datetime64', 'date'\] and have consistent types, but found {present_types}. Please use ToDatetimeTransformer to standardise"
+        with pytest.raises(
+            TypeError,
+            match=msg,
+        ):
+            x.transform(df)
 
 
 class TestInit(ColumnStrListInitTests):
--- tests/dates/test_BaseTwocolumnDateTransformer.py
+++ tests/dates/test_BaseTwocolumnDateTransformer.py
@@ -1,16 +1,39 @@
-from tests.base_tests import (
+import pytest
+
+from tests.dates.test_BaseDateTransformer import (
     ColumnStrListInitTests,
+    GenericDatesTransformTests,
     GenericFitTests,
-    GenericTransformTests,
     OtherBaseBehaviourTests,
 )
 
 
-class GenericTwoColumnDatesTransformTests:
-    """Generic tests for Dates Transformers"""
+class GenericTwoColumnDatesInitTests:
+    """Generic tests for Init Dates Transformers which take two columns"""
+
+    @pytest.mark.parametrize("columns", [["a"], ["a", "b", "c"]])
+    def test_not_two_columns_error(
+        self,
+        uninitialized_transformers,
+        minimal_attribute_dict,
+        columns,
+    ):
+        """ "test that two correct error raised when passed more or less than two columns"""
+
+        transformer = uninitialized_transformers[self.transformer_name]
+        init_args = minimal_attribute_dict[self.transformer_name]
+        init_args[columns] = columns
+
+        msg = f"{self.classname()}: This transformer works with two columns only"
+
+        with pytest.raises(
+            ValueError,
+            match=msg,
+        ):
+            transformer(init_args)
 
 
-class TestInit(ColumnStrListInitTests):
+class TestInit(ColumnStrListInitTests, GenericTwoColumnDatesInitTests):
     """Generic tests for transformer.init()."""
 
     @classmethod
@@ -26,7 +49,7 @@ class TestFit(GenericFitTests):
         cls.transformer_name = "BaseDateTwoColumnTransformer"
 
 
-class TestTransform(GenericTransformTests, GenericTwoColumnDatesTransformTests):
+class TestTransform(GenericDatesTransformTests):
     """Tests for BaseTwoColumnDateTransformer.transform."""
 
     @classmethod
