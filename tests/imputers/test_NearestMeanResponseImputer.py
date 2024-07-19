cffc8576ce8163bd4908896d58bf857c18f86b5a
Sam Forward <122817027+Chip2916@users.noreply.github.com>
2024-07-19 12:25:58 +0000

    Brough NMI transform tests in line with new framework

--- tests/imputers/test_NearestMeanResponseImputer.py
+++ tests/imputers/test_NearestMeanResponseImputer.py
@@ -9,6 +9,10 @@ import tests.test_data as d
 from tests.base_tests import (
     ColumnStrListInitTests,
     GenericFitTests,
+    GenericTransformTests,
+)
+from tests.imputers.test_BaseImputer import (
+    GenericImputerTransformTests,
 )
 from tubular.imputers import NearestMeanResponseImputer
 
@@ -154,161 +158,12 @@ class TestFit(GenericFitTests):
         )
 
 
-# class TestTransform:
-#     """Tests for NearestMeanResponseImputer.transform."""
+class TestTransform(
+    GenericTransformTests,
+    GenericImputerTransformTests,
+):
+    """Tests for transformer.transform."""
 
-#     def expected_df_1():
-#         """Expected output for ."""
-#         df = pd.DataFrame(
-#             {"a": [1, 1, 2, 3, 3, 2], "b": [3, 3, 1, 3, 3, 4], "c": [2, 3, 2, 1, 4, 1]},
-#         )
-
-#         df[["a", "b"]] = df[["a", "b"]].astype("float64")
-
-#         return df
-
-#     def expected_df_2():
-#         """Expected output for ."""
-#         df = pd.DataFrame(
-#             {
-#                 "a": [1, 1, 2, 3, 3, 2],
-#                 "b": [np.nan, np.nan, 1, 3, 3, 4],
-#                 "c": [2, 3, 2, 1, 4, 1],
-#             },
-#         )
-
-#         df["a"] = df["a"].astype("float64")
-
-#         return df
-
-#     def expected_df_3():
-#         """Expected output for ."""
-#         df = pd.DataFrame({"a": [2, 3, 4, 1, 4, 2]})
-
-#         df["a"] = df["a"].astype("float64")
-
-#         return df
-
-#     def test_check_is_fitted_called(self, mocker):
-#         """Test that BaseTransformer check_is_fitted called."""
-#         df = create_NearestMeanResponseImputer_test_df()
-
-#         x = NearestMeanResponseImputer(columns=["a", "b"])
-
-#         x.fit(df, df["c"])
-
-#         expected_call_args = {0: {"args": (["impute_values_"],), "kwargs": {}}}
-
-#         with ta.functions.assert_function_call(
-#             mocker,
-#             tubular.base.BaseTransformer,
-#             "check_is_fitted",
-#             expected_call_args,
-#         ):
-#             x.transform(df)
-
-#     def test_super_transform_called(self, mocker):
-#         """Test that BaseTransformer.transform called."""
-#         df = create_NearestMeanResponseImputer_test_df()
-
-#         x = NearestMeanResponseImputer(columns=["a", "b"])
-
-#         x.fit(df, df["c"])
-
-#         expected_call_args = {
-#             0: {"args": (create_NearestMeanResponseImputer_test_df(),), "kwargs": {}},
-#         }
-
-#         with ta.functions.assert_function_call(
-#             mocker,
-#             tubular.base.BaseTransformer,
-#             "transform",
-#             expected_call_args,
-#         ):
-#             x.transform(df)
-
-#     @pytest.mark.parametrize(
-#         ("df", "expected"),
-#         ta.pandas.adjusted_dataframe_params(
-#             create_NearestMeanResponseImputer_test_df(),
-#             expected_df_1(),
-#         ),
-#     )
-#     def test_nulls_imputed_correctly(self, df, expected):
-#         """Test missing values are filled with the correct values."""
-#         x = NearestMeanResponseImputer(columns=["a", "b"])
-
-#         # set the impute values dict directly rather than fitting x on df so test works with helpers
-#         x.impute_values_ = {"a": 2.0, "b": 3.0}
-
-#         df_transformed = x.transform(df)
-
-#         ta.equality.assert_equal_dispatch(
-#             expected=expected,
-#             actual=df_transformed,
-#             msg="Check nulls filled correctly in transform",
-#         )
-
-#     @pytest.mark.parametrize(
-#         ("df", "expected"),
-#         ta.pandas.adjusted_dataframe_params(
-#             create_NearestMeanResponseImputer_test_df(),
-#             expected_df_2(),
-#         ),
-#     )
-#     def test_nulls_imputed_correctly2(self, df, expected):
-#         """Test missing values are filled with the correct values - and unrelated columns are unchanged."""
-#         x = NearestMeanResponseImputer(columns="a")
-
-#         # set the impute values dict directly rather than fitting x on df so test works with helpers
-#         x.impute_values_ = {"a": 2.0}
-
-#         df_transformed = x.transform(df)
-
-#         ta.equality.assert_equal_dispatch(
-#             expected=expected,
-#             actual=df_transformed,
-#             msg="Check nulls filled correctly in transform",
-#         )
-
-#     @pytest.mark.parametrize(
-#         ("df", "expected"),
-#         ta.pandas.adjusted_dataframe_params(
-#             pd.DataFrame({"a": [np.nan, 3, 4, 1, 4, np.nan]}),
-#             expected_df_3(),
-#         ),
-#     )
-#     def test_nulls_imputed_correctly3(self, df, expected):
-#         """Test missing values are filled with the correct values - with median value from separate dataframe."""
-#         x = NearestMeanResponseImputer(columns="a")
-
-#         # set the impute values dict directly rather than fitting x on df so test works with helpers
-#         x.impute_values_ = {"a": 2.0}
-
-#         df_transformed = x.transform(df)
-
-#         ta.equality.assert_equal_dispatch(
-#             expected=expected,
-#             actual=df_transformed,
-#             msg="Check nulls filled correctly in transform",
-#         )
-
-#     def test_learnt_values_not_modified(self):
-#         """Test that the impute_values_ from fit are not changed in transform."""
-#         df = create_NearestMeanResponseImputer_test_df()
-
-#         x = NearestMeanResponseImputer(columns=["a", "b"])
-
-#         x.fit(df, df["c"])
-
-#         x2 = NearestMeanResponseImputer(columns=["a", "b"])
-
-#         x2.fit(df, df["c"])
-
-#         x2.transform(df)
-
-#         ta.equality.assert_equal_dispatch(
-#             expected=x.impute_values_,
-#             actual=x2.impute_values_,
-#             msg="Impute values not changed in transform",
-#         )
+    @classmethod
+    def setup_class(cls):
+        cls.transformer_name = "NearestMeanResponseImputer"
