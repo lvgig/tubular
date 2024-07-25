a032ba269ebed58478835901ef750b2c87d1bbcc
davidhopkinson26 <89029024+davidhopkinson26@users.noreply.github.com>
2024-07-25 14:07:20 +0000

    :art: changed to use tests from TwoColumnListInitTests and made Date sepcific test class a mixin

--- tests/dates/test_BaseTwocolumnDateTransformer.py
+++ tests/dates/test_BaseTwocolumnDateTransformer.py
@@ -1,40 +1,13 @@
-import pytest
-
-from tests.dates.test_BaseDateTransformer import (
-    ColumnStrListInitTests,
-    GenericDatesTransformTests,
+from tests.base_tests import (
     GenericFitTests,
+    GenericTransformTests,
     OtherBaseBehaviourTests,
+    TwoColumnListInitTests,
 )
+from tests.dates.test_BaseDateTransformer import DatesMixinTransformTests
 
 
-class GenericTwoColumnDatesInitTests:
-    """Generic tests for Init Dates Transformers which take two columns"""
-
-    @pytest.mark.parametrize("columns", ["a", ["a", "b", "c"]])
-    def test_not_two_columns_error(
-        self,
-        uninitialized_transformers,
-        minimal_attribute_dict,
-        columns,
-    ):
-        """ "test that two correct error raised when passed more or less than two columns"""
-
-        init_args = minimal_attribute_dict[self.transformer_name]
-        print(init_args)
-        init_args["columns"] = columns
-        print(init_args)
-
-        msg = f"{self.transformer_name}: This transformer works with two columns only"
-
-        with pytest.raises(
-            ValueError,
-            match=msg,
-        ):
-            uninitialized_transformers[self.transformer_name](*init_args)
-
-
-class TestInit(ColumnStrListInitTests, GenericTwoColumnDatesInitTests):
+class TestInit(TwoColumnListInitTests):
     """Generic tests for transformer.init()."""
 
     @classmethod
@@ -50,7 +23,7 @@ class TestFit(GenericFitTests):
         cls.transformer_name = "BaseDateTwoColumnTransformer"
 
 
-class TestTransform(GenericDatesTransformTests):
+class TestTransform(GenericTransformTests, DatesMixinTransformTests):
     """Tests for BaseTwoColumnDateTransformer.transform."""
 
     @classmethod
