34272db89912cb7b7c6bd60369704a3dd5591abd
Justin Staerck <51384727+alphanumericmale@users.noreply.github.com>
2024-10-08 14:37:22 +0000

    removed redundant tests

--- tests/numeric/test_CutTransformer.py
+++ tests/numeric/test_CutTransformer.py
@@ -1,5 +1,3 @@
-import re
-
 import pandas as pd
 import pytest
 import test_aide as ta
@@ -18,47 +16,6 @@ class TestInit(BaseNumericTransformerInitTests):
     @classmethod
     def setup_class(cls):
         cls.transformer_name = "CutTransformer"
-
-    def test_column_type_error(self):
-        """Test that an exception is raised if column is not a str."""
-        with pytest.raises(
-            TypeError,
-            match=re.escape(
-                "CutTransformer: column arg (name of column) should be a single str giving the column to discretise",
-            ),
-        ):
-            CutTransformer(
-                column=["a"],
-                new_column_name="a",
-            )
-
-    def test_new_column_name_type_error(self):
-        """Test that an exception is raised if new_column_name is not a str."""
-        with pytest.raises(
-            TypeError,
-            match="CutTransformer: new_column_name must be a str",
-        ):
-            CutTransformer(column="b", new_column_name=1)
-
-    def test_cut_kwargs_type_error(self):
-        """Test that an exception is raised if cut_kwargs is not a dict."""
-        with pytest.raises(
-            TypeError,
-            match=r"""cut_kwargs should be a dict but got type \<class 'int'\>""",
-        ):
-            CutTransformer(column="b", new_column_name="a", cut_kwargs=1)
-
-    def test_cut_kwargs_key_type_error(self):
-        """Test that an exception is raised if cut_kwargs has keys which are not str."""
-        with pytest.raises(
-            TypeError,
-            match=r"""CutTransformer: unexpected type \(\<class 'int'\>\) for cut_kwargs key in position 1, must be str""",
-        ):
-            CutTransformer(
-                new_column_name="a",
-                column="b",
-                cut_kwargs={"a": 1, 2: "b"},
-            )
 
 
 class TestTransform(BaseNumericTransformerTransformTests):
